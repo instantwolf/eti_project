e2cf1bbffabb2a2fc2d2659e899e173fe93d9735
Wolfgang Prett <wolfi-92@gmx.net>
2024-01-02 19:21:10 +0100

    added NFA class members, added simple logic, added finalizedLogic

---
+++ src/main/java/ab1/NFA.java
@@ -0,0 +1,117 @@
+package ab1;
+
+import java.util.Collection;
+
+/**
+ * The {@code NFA} interface represents a Non-deterministic Finite Automaton (NFA) and provides methods to manipulate
+ * NFAs as well as run words against the NFA to check if they are in the language or not. The NFA has two states:
+ * edit and accept. While in edit-state, new transitions and accepting states can be added until
+ * {@link #finalizeAutomaton()} is called. If a method is called in the wrong state, a {@link FinalizedStateException}
+ * should be thrown.
+ */
+public interface NFA {
+
+    /**
+     * @return all states present in the automaton
+     */
+    Collection<String> getStates();
+
+    /**
+     * @return all transitions currently configured in the automaton
+     */
+    Collection<Transition> getTransitions();
+
+    /**
+     * @return all states present in the automata, marked as accepting state
+     */
+    Collection<String> getAcceptingStates();
+
+    /**
+     * @return initial state of the automata. This cannot change over the lifetime of the object
+     */
+    String getInitialState();
+
+    /**
+     * Add a new transition to the automata.
+     *
+     * @param transition information about the new transition. If the automaton already has a transition for the
+     *                   given fromState and readSymbol, it should be added nonetheless.
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was already called
+     */
+    void addTransition(Transition transition) throws FinalizedStateException;
+
+    /**
+     * Flag a state to be an accepting state. If the state was accepting before, nothing changes.
+     *
+     * @param state - label of the new accepting state
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was already called
+     */
+    void addAcceptingState(String state) throws FinalizedStateException;
+
+    /**
+     * @param other any NFA
+     * @return a new NFA instance that accepts the language L(return) = L(this) ∪ L(other).
+     * other. Both, other and this NFA must not change during this operation.
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was not already called
+     */
+    NFA union(NFA other) throws FinalizedStateException;
+
+    /**
+     * @param other - any NFA
+     * @return a new NFA instance that accepts the language L(return) = L(this) ∩ L(other).
+     * Both, other and this NFA must not change during this operation.
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was not already called
+     */
+    NFA intersection(NFA other) throws FinalizedStateException;
+
+    /**
+     * @param other - any NFA
+     * @return a new NFA instance that accepts the language L(return) = L(this) + L(other).
+     * Both, other and this NFA must not change during this operation.
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was not already called
+     */
+    NFA concatenation(NFA other) throws FinalizedStateException;
+
+    /**
+     * @return a new NFA instance that accepts the language L(this)*.
+     * This NFA must not change during the operation.
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was not already called
+     */
+    NFA kleeneStar() throws FinalizedStateException;
+
+    /**
+     * @return a new NFA instance that accepts the language L(this)⁺.
+     * This NFA must not change during the operation.
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was not already called
+     */
+    NFA plusOperator() throws FinalizedStateException;
+
+    /**
+     * @return a new NFA instance that accepts the complement of L(this).
+     * This NFA must not change during the operation.
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was not already called
+     */
+    NFA complement() throws FinalizedStateException;
+
+    /**
+     * @return if the automaton is editable or not
+     */
+    boolean isFinalized();
+
+    /**
+     * Marks the automaton as finalized. Only now words can be tested.
+     */
+    void finalizeAutomaton();
+
+    /**
+     * @return whether the language of this automaton is finite or infinite.
+     */
+    boolean isFinite();
+
+    /**
+     * @param word - word to check, can be empty
+     * @return true, iff the word lies in the language of the automaton
+     * @throws FinalizedStateException if {@link #finalizeAutomaton()} was not already called
+     */
+    boolean acceptsWord(String word) throws FinalizedStateException;
+}
---
+++ src/main/java/ab1/impl/GRUPPE/NFAImpl.java
@@ -0,0 +1,183 @@
+package ab1.impl.GRUPPE;
+
+import ab1.FinalizedStateException;
+import ab1.NFA;
+import ab1.Transition;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Implicit assumptions:
+ *  - the alphabet is every single Character of the Character Datatype
+ *    class including null (for eps) <= Transition-Record
+ *      - it follows that: every String of length 1 including space
+ *        is an element of the alphabet and hence is not a valid name
+ *        for any state (diff datatype is not distinguished enough)
+ *  - star1Test => in the context of strings , empty means epsilon (since a character cannot be
+ *    empty , but a string can)
+ *      - it follows that:
+ *              every state must be of length >1
+ *      -       no String can be NULL since epsilon is empty String
+ *  - finalization: according to the tests (finalizeTests) the following applies:
+ *          - after initialization an automaton is not finalized
+ *          - an automaton is finalized once finalize method is called
+ *          - a non-finalized automaton can be structurally modified (states /transitions) , a finalized one cannot
+ *          - mathematical operations require all involved automatons to be finalized (callee as well as input parameters)
+ *  - adding states:
+ *          - not explicitly (publicly) implemented because tests require implicit adding of states through addTransition
+ */
+public class NFAImpl implements NFA {
+
+    /** Member Var */
+    Set<String> states;
+    Set<Transition> transitions;
+    Set<String> acceptingStates;
+    String initialState;
+    boolean isFinalized;
+
+    /** initialization */
+    public NFAImpl(String startState) {
+        initMembers();
+        setInitialState(startState);
+    }
+    private void initMembers(){
+        states = new HashSet<>();
+        transitions = new HashSet<>();
+        acceptingStates = new HashSet<>();
+        isFinalized = false;
+    }
+
+
+    /** getter / setter */
+    @Override
+    public boolean isFinalized() {
+        return this.isFinalized;
+    }
+
+    @Override
+    public void finalizeAutomaton() {
+        this.isFinalized = true;
+    }
+
+    @Override
+    public Set<String> getStates() {
+        return this.states;
+    }
+
+    @Override
+    public Collection<Transition> getTransitions() {
+        return this.transitions;
+    }
+
+    @Override
+    public Set<String> getAcceptingStates() {
+        return this.acceptingStates;
+    }
+
+    @Override
+    public String getInitialState() {
+        return this.initialState;
+    }
+
+    @Override
+    public void addTransition(Transition transition) throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(false);
+        checkValidTransition(transition);
+        //add states (if they arent part of the state-set)
+        this.transitions.add(transition);
+    }
+
+    @Override
+    public void addAcceptingState(String state) throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(false);
+        this.acceptingStates.add(state);
+    }
+
+    /** operations on NFAs */
+    @Override
+    public NFA union(NFA other) throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(true);
+        return null;
+    }
+
+    @Override
+    public NFA intersection(NFA other) throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(true);
+        return null;
+    }
+
+    @Override
+    public NFA concatenation(NFA other) throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(true);
+        return null;
+    }
+
+    @Override
+    public NFA kleeneStar() throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(true);
+        return null;
+    }
+
+    @Override
+    public NFA plusOperator() throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(true);
+        return null;
+    }
+
+    @Override
+    public NFA complement() throws FinalizedStateException {
+        throwExceptionIfFinalizedDeviates(true);
+        return null;
+    }
+
+
+    /** status methods */
+    @Override
+    public boolean isFinite() {
+        return false;
+    }
+
+    @Override
+    public boolean acceptsWord(String word) {
+        return false;
+    }
+
+    /** Helper Methods */
+    private void checkValidState(String state) throws IllegalArgumentException{
+        if(state == null || state.length() < 2){
+            throw new IllegalArgumentException("State cannot be null , empty string or single character");
+        }
+    }
+
+    private void setInitialState(String state) throws IllegalArgumentException, FinalizedStateException{
+        throwExceptionIfFinalizedDeviates(false);
+        addState(state);
+        this.initialState = state;
+    }
+
+    private void addState(String state) throws IllegalArgumentException, FinalizedStateException{
+      //  throwExceptionIfFinalizedDeviates(false);
+        checkValidState(state);
+        this.states.add(state);
+    }
+
+    private void throwExceptionIfFinalizedDeviates(boolean requiredFinalizedState) throws FinalizedStateException{
+        if(this.isFinalized != requiredFinalizedState){
+            throw new FinalizedStateException(); //not allowed when NFA is finalized
+        }
+    }
+
+    private void checkValidTransition(Transition test){
+        checkValidState(test.fromState());
+        checkValidState(test.toState());
+        //the character in the transition cannot be invalid since there arent any restrictions from the assignment
+        //and we arent allowed to just make some up
+        //can be null (eps) or any character
+
+        addState(test.fromState());
+        addState(test.toState());
+    }
+
+}
